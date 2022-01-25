c0c5d84348f9da703c645ea9e9fabc585a6ee79a
LeonardoAleix0 <86136535+LeonardoAleix0@users.noreply.github.com>
2022-01-25 14:55:08 -0500

    adding queries

--- queries.sql
+++ queries.sql
@@ -15,7 +15,8 @@ Select          location,
 		total_cases AS total_cases,
 		(total_cases/population)*100 AS percerntage_of_cases
 From            covid_continent
-WHERE           location in (‘Africa’, ‘Asia’, ‘Europe’, ‘North America’, ‘South America’, ‘Oceania’)
+WHERE           location in (‘Africa’, ‘Asia’, ‘Europe’, 
+                ‘North America’, ‘South America’, ‘Oceania’)
 AND		date = ‘2022-01-09’ 	
 GROUP BY        location, population, date, total_cases
 ORDER BY        percerntage_of_cases desc
