110851f46917f5eef540d03d78bbc63193477151
LeonardoAleix0 <86136535+LeonardoAleix0@users.noreply.github.com>
2022-01-29 17:42:43 -0500

    reworking schemas

--- schema_covid_countries.sql
+++ schema_covid_countries.sql
@@ -1,10 +1,11 @@
 -- Creating tables for Covid-19 Project
 
 CREATE TABLE covid_countries (	
+
 	ind_con NUMERIC,
 	iso_code VARCHAR (40),
 	continent TEXT,
-    location TEXT,
+    	location TEXT,
 	date DATE,
 	population NUMERIC,	
 	total_cases NUMERIC,
@@ -12,12 +13,12 @@ CREATE TABLE covid_countries (
 	new_cases_smoothed FLOAT8,
 	total_deaths NUMERIC,
 	new_deaths NUMERIC,	
-    new_deaths_smoothed	FLOAT8,
-    total_cases_per_million FLOAT8,
-    new_cases_per_million FLOAT8,
+    	new_deaths_smoothed	FLOAT8,
+    	total_cases_per_million FLOAT8,
+    	new_cases_per_million FLOAT8,
 	new_cases_smoothed_per_million FLOAT8,
 	total_deaths_per_million FLOAT8,
-    new_deaths_per_million FLOAT8,
+    	new_deaths_per_million FLOAT8,
 	new_deaths_smoothed_per_million FLOAT8,
 	reproduction_rate FLOAT8,
 	icu_patients NUMERIC,
@@ -25,9 +26,11 @@ CREATE TABLE covid_countries (
 	hosp_patients NUMERIC,
 	hosp_patients_per_million FLOAT8,
 	weekly_icu_admissions NUMERIC,	
-    weekly_icu_admissions_per_million FLOAT8,
+   	weekly_icu_admissions_per_million FLOAT8,
 	weekly_hosp_admissions NUMERIC,
-	weekly_hosp_admissions_per_million FLOAT8
+	weekly_hosp_admissions_per_million FLOAT8,
+	excess_mortality_cumulative_per_million FLOAT8
+	
 
 );
 
--- schema_covid_vaccinations.sql
+++ schema_covid_vaccinations.sql
@@ -1,30 +1,31 @@
 -- Creating tables for Covid-19 Project
 
 CREATE TABLE covid_vaccinations (	
-    ind_vac NUMERIC,
-    iso_code VARCHAR (40),
+
+    	ind_vac NUMERIC,
+    	iso_code VARCHAR (40),
 	continent TEXT,
 	location TEXT,
 	date DATE,
-    new_tests NUMERIC,	
+    	new_tests NUMERIC,	
 	total_tests NUMERIC,
-    total_tests_per_thousand FLOAT8,
+    	total_tests_per_thousand FLOAT8,
 	new_tests_per_thousand FLOAT8,
 	new_tests_smoothed NUMERIC,
 	new_tests_smoothed_per_thousand FLOAT8,
-    positive_rate FLOAT8,
+    	positive_rate FLOAT8,
 	tests_per_case FLOAT8,	
 	tests_units TEXT,
 	total_vaccinations BIGINT,
 	people_vaccinated BIGINT,
  	people_fully_vaccinated BIGINT,	 
 	total_boosters BIGINT,
-	new_vaccinations BIGINT
+	new_vaccinations BIGINT,
 	new_vaccinations_smoothed BIGINT,
 	total_vaccinations_per_hundred FLOAT8,
 	people_vaccinated_per_hundred FLOAT8,
 	people_fully_vaccinated_per_hundred FLOAT8,
-    total_boosters_per_hundred FLOAT8,
+   	total_boosters_per_hundred FLOAT8,
 	new_vaccinations_smoothed_per_million BIGINT,
 	new_people_vaccinated_smoothed BIGINT,
 	new_people_vaccinated_smoothed_per_hundred FLOAT8,
@@ -39,14 +40,14 @@ CREATE TABLE covid_vaccinations (
 	diabetes_prevalence FLOAT8,
 	female_smokers FLOAT8,
 	male_smokers FLOAT8,
-    handwashing_facilities FLOAT8,
-    hospital_beds_per_thousand FLOAT8,
+    	handwashing_facilities FLOAT8,
+    	hospital_beds_per_thousand FLOAT8,
 	life_expectancy FLOAT8,
 	human_development_index FLOAT8,
-	excess_mortality_cumulative_absolute BIGINT
+	excess_mortality_cumulative_absolute FLOAT8,
 	excess_mortality_cumulative FLOAT8,
 	excess_mortality FLOAT8,
-    excess_mortality_cumulative_per_million FLOAT8,
+    	excess_mortality_cumulative_per_million FLOAT8
 	);
 
 
